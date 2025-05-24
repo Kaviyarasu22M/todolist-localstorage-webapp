8679d0ea4d919e4ef6651e8a09a6aa544080b163
Kaviyarasu M <mkaviyarasu81@gmail.com>
2025-05-24 22:44:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let input = document.getElementById("input-box");
+let btn = document.getElementById("buttonn");
+let adlist = document.getElementById("addlist");
+
+let todos = [];
+
+window.onload = () => {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
+  todos.forEach((task) => renderTask(task));  
+};
+
+btn.addEventListener("click", () => {
+  let taskValue = input.value.trim();
+  if (taskValue !== "") {
+    todos.push(taskValue); 
+    localStorage.setItem("todos", JSON.stringify(todos));
+    renderTask(taskValue); 
+    input.value = "";
+  }
+});
+
+function renderTask(task) {
+  let h33add = document.createElement("h3");
+  h33add.innerHTML = task;
+  h33add.style.backgroundColor = "rgb(196, 189, 189)";
+  h33add.style.height = "40px";
+  h33add.style.margin = "10px";
+  adlist.appendChild(h33add);
+
+  let dell = document.createElement("span");
+  dell.innerHTML = `&#10006;`;
+  dell.title = "Delete task";
+  dell.style.marginLeft = "20px";
+  dell.style.cursor = "pointer";
+  h33add.appendChild(dell);
+
+  dell.addEventListener("click", () => {
+    adlist.removeChild(h33add);
+    removee(task);
+  });
+}
+
+function removee(taskk) {
+  let index = todos.indexOf(taskk);
+  if (index !== -1) {
+    todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+}
